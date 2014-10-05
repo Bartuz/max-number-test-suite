820b653c9b571f1b4b10b6cdabef3976b1f7064a
Bartuz <filip@bartuzi.me>
2014-10-05 22:41:19 +0200

    Add more test to if implementation

--- spec/lib/array_decorator_spec.rb
+++ spec/lib/array_decorator_spec.rb
@@ -12,33 +12,72 @@ describe Array do
       end
     end
 
-    context 'when the first number is maximum' do
-      subject(:array) { [3, 1, 2] }
-
-      it 'returns maximum number' do
-        expect(array.maximum_if_implementation).to eq(3)
-      end
-    end
-
-    context 'when the middle number is maximum' do
-      subject(:array) { [1, 3, 2] }
-
-      it 'returns maximum number' do
-        expect(array.maximum_if_implementation).to eq(3)
-      end
-    end
-
-    context 'when the last number is maximum' do
+    context 'when supported number of elements is given' do
       subject(:array) { [1, 2, 3] }
 
-      it 'returns maximum number' do
-        expect(array.maximum_if_implementation).to eq(3)
+      it 'does not raise an error' do
+        expect { array.maximum_if_implementation }.not_to raise_error
+      end
+
+      context 'when all elements are different' do
+
+        context 'when the first number is maximum' do
+          subject(:array) { [3, 1, 2] }
+          let(:expected_maximum) { 3 }
+
+          it 'returns maximum number' do
+            expect(array.maximum_if_implementation).to eq(expected_maximum)
+          end
+        end
+
+        context 'when the middle number is maximum' do
+          subject(:array) { [1, 3, 2] }
+          let(:expected_maximum) { 3 }
+
+          it 'returns maximum number' do
+            expect(array.maximum_if_implementation).to eq(expected_maximum)
+          end
+        end
+
+        context 'when the last number is maximum' do
+          subject(:array) { [1, 2, 3] }
+          let(:expected_maximum) { 3 }
+
+          it 'returns maximum number' do
+            expect(array.maximum_if_implementation).to eq(expected_maximum)
+          end
+        end
       end
     end
 
+    context 'when are elements are the same' do
+      subject(:array) { [1, 1, 1] }
+      let(:expected_maximum) { 1 }
+
+      it 'returns maximum number' do
+        expect(array.maximum_if_implementation).to eq(expected_maximum)
+      end
+
+      it 'returns single number' do
+        expect(array.maximum_if_implementation).to be_kind_of Fixnum
+      end
+    end
+
+    context 'when two elements are the same' do
+      subject(:array) { [1, 1, 2] }
+      let(:expected_maximum) { 2 }
+
+      it 'returns maximum number' do
+        expect(array.maximum_if_implementation).to eq(expected_maximum)
+      end
+
+      it 'returns single number' do
+        expect(array.maximum_if_implementation).to be_kind_of Fixnum
+      end
+    end
   end
 
-  describe '#maximum_number_algorithm_2' do
+  describe '#maximum_while_implementation' do
     xit 'returns meximum number' do
       expect(array.maximum_number_algorithm_2).to eq(3)
     end
